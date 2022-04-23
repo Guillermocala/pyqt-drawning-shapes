df9588bab132a5b2a00f9320512a2c977ae81b79
guillermo cala <guillermo.cala7@gmail.com>
2022-04-23 18:07:08 -0500

    status: -añadida gestion de datos -correciones en la manipulacion del toolbar y context menu -dibujado mejorado

--- main.py
+++ main.py
@@ -21,14 +21,20 @@ class MyApp(QMainWindow):
         self.initMenuBar()
         self.initToolbar()
         self.show()
+        """para deshabilitar la molesta opcion de hide toolbar cuando se da 
+        click derecho, deshabilita los context menu en general"""
+        self.setContextMenuPolicy(Qt.PreventContextMenu)
+        self.indexState = 1
+        self.main_dictionary = {0:QPoint(100, 100)}
+        self.states_dictionary = {}
+        self.accepted_states_dictionary = {}
 
     def initUI(self):
         # lista de puntos para dibujar
+        self.posx = 200
+        self.posy = 200
         self.points = {1:QPoint(100, 200), 2:QPoint(150, 250), 3:QPoint(200, 300)}
         print(self.points)
-        self.draw = False
-        self.square = False
-        self.circle = False
         self.lienzo = QLabel()
         self.pixmap = QPixmap(self.size())
         self.painter = QPainter(self.pixmap)
@@ -51,54 +57,61 @@ class MyApp(QMainWindow):
         self.opcion1.addAction("Exit", self.close)
         #opcion2
         self.opcion2 = self.menu_bar.addMenu("Toolbar")
-        self.subAction21 = self.opcion2.addAction("Option 1")
+        self.subAction21 = self.opcion2.addAction("Option 1", self.toolbarShow)
         self.subAction22 = self.opcion2.addAction("Option 2")
+        self.subAction21.setCheckable(True)
+        self.subAction22.setCheckable(True)
 
     def initToolbar(self):
+        self.subAction21.setChecked(True)
         self.options_toolbar = QToolBar("Options1")
+        self.options_toolbar.setMovable(False)
         self.addToolBar(Qt.RightToolBarArea, self.options_toolbar)
-        self.options_toolbar.addAction("Draw state", self.saludo)
-        self.options_toolbar.addAction("Draw accept state", self.saludo)
+        self.options_toolbar.addAction("Draw state", self.drawStates)
+        self.options_toolbar.addAction("Draw accept state", self.drawStates)
         self.options_toolbar.addSeparator()
-        self.options_toolbar.addAction("testing")
-        self.button_action = QAction("circle", self)
-        self.button_action2 = QAction("square", self)
-        self.button_action.triggered.connect(self.onMyToolBarButtonClick)
-        self.button_action2.triggered.connect(self.onMyToolBarButtonClick)
-        self.options_toolbar.addAction(self.button_action)
-        self.options_toolbar.addAction(self.button_action2)
+        self.options_toolbar.addAction("Clear screen", self.clearScreen)
+        self.options_toolbar.addAction("Show data", self.showData)
     
-    def onMyToolBarButtonClick(self):
-        print(self.sender().text())
-        self.pen2 = QPen(Qt.red, 2, Qt.SolidLine)
-        self.painter.setPen(self.pen2)
-        match self.sender().text():
-            case "circle":
-                if self.circle:
-                    self.circle = False
-                    self.pixmap.fill(Qt.white)
-                else:
-                    # draw a accept state
-                    self.painter.drawEllipse(200, 200, 50, 50)
-                    self.painter.drawEllipse(195, 195, 60, 60)
-                    self.circle = True
-            case "square":
-                if self.square:
-                    self.square = False
-                    self.pixmap.fill(Qt.white)
-                else:
-                    self.painter.drawRect(200, 200, 200, 200)
-                    self.square = True
-
-    def saludo(self):
-        self.painter.setPen(self.pen)
-        if self.draw:
-            self.draw = False
-            self.pixmap.fill(Qt.white)
+    def toolbarShow(self):
+        if self.options_toolbar.isVisible():
+            self.options_toolbar.setVisible(False)
         else:
-            for key, value in self.points.items():
-                self.painter.drawEllipse(value, 50, 50)
-            self.draw = True
+            self.options_toolbar.setVisible(True)
+
+    def drawStates(self):
+        point = QPoint((self.posx), (self.posy))
+        self.posx += 50     
+        match self.sender().text():
+            case "Draw state":
+                print("Draw state case")
+                # draw a accept state
+                self.main_dictionary[self.indexState] = point
+                self.states_dictionary[self.indexState] = point
+                self.indexState += 1
+                self.painter.drawEllipse(point, 50, 50)
+            case "Draw accept state":
+                print("Draw accept state case")
+                self.main_dictionary[self.indexState] = point
+                self.accepted_states_dictionary[self.indexState] = point
+                self.indexState += 1
+                self.painter.drawEllipse(point, 50, 50)
+                self.painter.drawEllipse(point, 60, 60)
+            case _:
+                print("Error option -> draw states match")
+        print(self.main_dictionary)
+
+    def clearScreen(self):
+        self.main_dictionary = {0:QPoint(100, 100)}
+        self.states_dictionary = {}
+        self.accepted_states_dictionary = {}
+        self.posx = 200
+        self.pixmap.fill(Qt.white)
+
+    def showData(self):
+        print("main dict: ", self.main_dictionary)
+        print("states dict: ", self.states_dictionary)
+        print("accepted states dict: ", self.accepted_states_dictionary)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
