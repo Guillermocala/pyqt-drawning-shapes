3d647027f9b32459e81ce0d737d76290459d02c7
guillermo cala <guillermo.cala7@gmail.com>
2022-05-08 22:07:32 -0500

    status: -añadido modulo dibujar transiciones -operacion dibujar transiciones por mejorar

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from PySide6.QtGui import (
 )
 from PySide6.QtWidgets import (
     QApplication, QMainWindow, QVBoxLayout, QWidget,
-    QLabel, QHBoxLayout, QToolBar
+    QLabel, QHBoxLayout, QToolBar, QComboBox, QPushButton
 )
 
 class MyApp(QMainWindow):
@@ -19,20 +19,25 @@ class MyApp(QMainWindow):
         self.setGeometry(300, 100, 800, 500)
         self.setWindowIcon(QIcon("graph.ico"))
         self.setWindowTitle("Shape drawer")
+        "tamaños de los circulos de los estados"
+        self.size_inner_circle = 25
+        self.size_outer_circle = 33
+        "indexState es el controlador central de mi lista"
+        self.indexState = 1
+        "listas de estados"
+        self.main_dictionary = {0:QPoint(100, 100)}
+        self.states_dictionary = {}
+        self.accepted_states_dictionary = {}
+        "inicializacion de componentes"
         self.initUI()
         self.initMenuBar()
+        self.initTransitionsModule()
         self.initToolbar()
         self.statusBar().showMessage("Welcome to the AFND visualizer", 5000)
         self.show()
         """para deshabilitar la molesta opcion de hide toolbar cuando se da 
         click derecho, deshabilita los context menu en general"""
         self.setContextMenuPolicy(Qt.PreventContextMenu)
-        self.indexState = 1
-        self.size_inner_circle = 25
-        self.size_outer_circle = 33
-        self.main_dictionary = {0:QPoint(100, 100)}
-        self.states_dictionary = {}
-        self.accepted_states_dictionary = {}
 
     def initUI(self):
         self.actual_pos = QPoint(0, 0)
@@ -66,6 +71,26 @@ class MyApp(QMainWindow):
         self.subAction21.setCheckable(True)
         self.subAction22.setCheckable(True)
 
+    "modulo para insertar las aristas o transiciones(2 combobox y un boton)"
+    def initTransitionsModule(self):
+        self.transitionsModule = QWidget()
+        self.layoutTransitionsModule = QVBoxLayout(self.transitionsModule)
+        self.layoutComboboxes = QHBoxLayout()
+        self.layoutButton = QVBoxLayout()
+        self.initialState = QComboBox()
+        self.endingState = QComboBox()
+        self.initialState.setPlaceholderText("State 1")
+        self.endingState.setPlaceholderText("State 2")
+        self.initialState.addItem("0")
+        self.endingState.addItem("0")
+        self.drawTransitionButton = QPushButton("Draw transition")
+        self.drawTransitionButton.clicked.connect(self.drawTransitions)
+        self.layoutComboboxes.addWidget(self.initialState)
+        self.layoutComboboxes.addWidget(self.endingState)
+        self.layoutButton.addWidget(self.drawTransitionButton)
+        self.layoutTransitionsModule.addLayout(self.layoutComboboxes)
+        self.layoutTransitionsModule.addLayout(self.layoutButton)
+
     def initToolbar(self):
         self.subAction21.setChecked(True)
         self.options_toolbar = QToolBar("Options1")
@@ -79,7 +104,8 @@ class MyApp(QMainWindow):
         self.draw_transition_action.triggered.connect(self.drawTransitions)
         self.options_toolbar.addAction(self.draw_state_action)
         self.options_toolbar.addAction(self.draw_accept_state_action)
-        self.options_toolbar.addAction(self.draw_transition_action)
+        self.options_toolbar.addSeparator()
+        self.options_toolbar.addWidget(self.transitionsModule)
         self.options_toolbar.addSeparator()
         self.options_toolbar.addAction("Clear screen", self.clearScreen)
         self.options_toolbar.addAction("Show data", self.showData)
@@ -115,7 +141,10 @@ class MyApp(QMainWindow):
                 self.states_dictionary[self.indexState] = self.actual_pos
                 self.painter.drawText(self.actual_pos, str(self.indexState))
                 self.painter.drawEllipse(self.actual_pos, self.size_inner_circle, self.size_inner_circle)
+                self.initialState.addItem(str(self.indexState))
+                self.endingState.addItem(str(self.indexState))
                 self.indexState += 1
+                self.statusBar().setStyleSheet("background-color:green")
                 self.statusBar().showMessage("STATUS:   State drawed!", 2000)
             case "Draw accept state":
                 print("Draw accept state case")
@@ -126,7 +155,10 @@ class MyApp(QMainWindow):
                 self.painter.drawText(self.actual_pos, str(self.indexState))
                 self.painter.drawEllipse(self.actual_pos, self.size_inner_circle, self.size_inner_circle)
                 self.painter.drawEllipse(self.actual_pos, self.size_outer_circle, self.size_outer_circle)
+                self.initialState.addItem(str(self.indexState))
+                self.endingState.addItem(str(self.indexState))
                 self.indexState += 1
+                self.statusBar().setStyleSheet("background-color:green")
                 self.statusBar().showMessage("STATUS:   Accept state drawed!", 2000)
             case _:
                 print("Error option -> draw states match")
@@ -134,36 +166,46 @@ class MyApp(QMainWindow):
         self.draw_accept_state_action.setEnabled(True)
 
     def drawTransitions(self):
-        path = QPainterPath()
-        print(self.main_dictionary[1])
-        print(self.main_dictionary[2])
-        punto1 = QPointF(self.main_dictionary[1])
-        punto2 = QPointF(self.main_dictionary[2])
-        path.moveTo(punto1)
-        restaPuntos = QPoint(punto1.x() - punto2.x(), punto1.y() - punto2.y())
-        if restaPuntos.x() < 0 and restaPuntos.y() > 0:
-            puntoControl1 = QPointF(punto1.x(), punto2.y())
-        elif restaPuntos.x() > 0 and restaPuntos.y() > 0:
-            puntoControl1 = QPointF(punto2.x(), punto1.y())
-        elif restaPuntos.x() > 0 and restaPuntos.y() < 0:
-            puntoControl1 = QPointF(punto1.x(), punto2.y())
-        elif restaPuntos.x() < 0 and restaPuntos.y() < 0:
-            puntoControl1 = QPointF(punto2.x(), punto1.y())
-        
-        path.quadTo(puntoControl1, punto2)
-        self.painter.drawPoint(puntoControl1)
-        self.painter.drawPath(path)
-        print("dibujado?")
-
+        primerSeleccionado = self.initialState.currentText()
+        segundoSeleccionado = self.endingState.currentText()
+        self.statusBar().setStyleSheet("background-color:red")
+        if primerSeleccionado != "" and segundoSeleccionado != "":
+            path = QPainterPath()
+            punto1 = QPointF(self.main_dictionary[int(primerSeleccionado)])
+            punto2 = QPointF(self.main_dictionary[int(segundoSeleccionado)])
+            if punto1 == punto2:
+                self.statusBar().showMessage("STATUS:   Building!", 5000)
+            else:
+                #path.moveTo(QPointF(punto1.x() - self.size_inner_circle, punto1.y() - self.size_inner_circle))
+                path.moveTo(punto1)
+                restaPuntos = QPoint(punto1.x() - punto2.x(), punto1.y() - punto2.y())
+                if restaPuntos.x() < 0 and restaPuntos.y() > 0:
+                    puntoControl1 = QPointF(punto1.x(), punto2.y())
+                elif restaPuntos.x() > 0 and restaPuntos.y() > 0:
+                    puntoControl1 = QPointF(punto2.x(), punto1.y())
+                elif restaPuntos.x() > 0 and restaPuntos.y() < 0:
+                    puntoControl1 = QPointF(punto1.x(), punto2.y())
+                elif restaPuntos.x() < 0 and restaPuntos.y() < 0:
+                    puntoControl1 = QPointF(punto2.x(), punto1.y())
+                path.quadTo(puntoControl1, punto2)
+                self.painter.drawPoint(puntoControl1)
+                self.painter.drawPath(path)
+                self.statusBar().setStyleSheet("background-color:green")
+                self.statusBar().showMessage("STATUS:   Transition Drawed!", 2000)
+        else:
+            self.statusBar().showMessage("STATUS:   Debe seleccionar dos indices!", 5000)
 
     def clearScreen(self):
+        "borramos las opciones de los combobox iterando al revés"
+        for i in range(len(self.main_dictionary) + 1, 0, -1):
+            self.initialState.removeItem(i)
+            self.endingState.removeItem(i)
         self.indexState = 1
         self.main_dictionary = {0:QPoint(100, 100)}
         self.states_dictionary = {}
         self.accepted_states_dictionary = {}
-        self.posx = 200
         self.pixmap.fill(Qt.white)
-        self.statusBar().setStyleSheet("background-color:yellow")
+        self.statusBar().setStyleSheet("background-color:green")
         self.statusBar().showMessage("STATUS:   Screen cleared!", 2000)
 
     def showData(self):
